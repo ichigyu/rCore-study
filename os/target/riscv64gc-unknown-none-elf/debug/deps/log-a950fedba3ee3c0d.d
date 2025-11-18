78d11d624e4ce4f8ae58053fba980e25a2e9a889
ichigyu <ichigyu88@gmail.com>
2025-11-18 12:00:51 +0800

    remove target, Cargo.lock, .idea

--- os/target/riscv64gc-unknown-none-elf/debug/deps/log-a950fedba3ee3c0d.d
+++
@@ -1,8 +0,0 @@
-/home/ichigyu/WorkSpace/rCore-study/os/target/riscv64gc-unknown-none-elf/debug/deps/log-a950fedba3ee3c0d.d: /home/ichigyu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/lib.rs /home/ichigyu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/macros.rs /home/ichigyu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/serde.rs /home/ichigyu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/__private_api.rs
-
-/home/ichigyu/WorkSpace/rCore-study/os/target/riscv64gc-unknown-none-elf/debug/deps/liblog-a950fedba3ee3c0d.rmeta: /home/ichigyu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/lib.rs /home/ichigyu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/macros.rs /home/ichigyu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/serde.rs /home/ichigyu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/__private_api.rs
-
-/home/ichigyu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/lib.rs:
-/home/ichigyu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/macros.rs:
-/home/ichigyu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/serde.rs:
-/home/ichigyu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/__private_api.rs:
